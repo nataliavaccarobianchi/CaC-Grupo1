e42d818a833f2d0a2d52264814a994f5411c946c
leo <leo@gmail.com>
2024-05-23 23:27:57 -0300

    agregado menú hamburguesa (falta corregir posición y estilos), faltan estilos iconos footer, y algunos detalles responsive

---
+++ script-menu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
+
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+});
