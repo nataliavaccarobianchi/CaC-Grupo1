5b04d13992501adc6e5a27c16fc01d8b01c9baf3
LeoGit999 <estudio.sagitarius@gmail.com>
2024-05-27 02:22:06 -0300

    nosotros.html y style_nosotros.css concluido v1

--- script-menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const navList = document.querySelector('.nav-list');
-
-    mobileMenu.addEventListener('click', () => {
-        navList.classList.toggle('active');
-    });
-});
